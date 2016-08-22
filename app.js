ef8a8b95037baba66ca198d47fcb9139da5e9df2
Senthil Chandran <senthilc@microsoft.com>
2016-08-21 22:25:22 -0700

    chat connetor

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 var builder = require('botbuilder');
+var restify = require('restify');
 
-var connector = new builder.ConsoleConnector().listen();
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.KNOW_SENTHIL_APP_ID,
+    appPassword: process.env.KNOW_SENTHIL_APP_PASSWORD
+});
+
 var bot = new builder.UniversalBot(connector);
-bot.dialog('/', [
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
     function (session, args, next) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
